03b9647c3c9e18aa97047876319b8ca69d5ad294
Shin Yamamoto <shin@scenee.com>
2018-11-24 10:54:10 +0900

    Fix a backdrop's cut off on orientation change

--- Framework/Sources/FloatingPanelView.swift
+++ Framework/Sources/FloatingPanelView.swift
@@ -9,10 +9,13 @@ class FloatingPanelPassThroughView: UIView {
     public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let view = super.hitTest(point, with: event)
         switch view {
-        case is FloatingPanelPassThroughView:
+        case is FloatingPanelPassThroughView,
+             is FloatingPanelSurfaceWrapperView:
             return nil
         default:
             return view
         }
     }
 }
+
+class FloatingPanelSurfaceWrapperView: UIView { }
